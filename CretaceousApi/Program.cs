78f6ad03a808cea9a46ee4da7f216f57813b87d6
NicksFed <nick.guzy76@gmail.com>
2023-03-27 14:29:15 -0700

    Add json file updates, csproj updates, Program.cs, comment out Weatherforecast controller and context, add CretaaceousApiContext and Animal Model.

--- CretaceousApi/Program.cs
+++ CretaceousApi/Program.cs
@@ -1,8 +1,21 @@
+using CretaceousApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<CretaceousApiContext>(
+                  dbContextOptions => dbContextOptions
+                    .UseMySql(
+                      builder.Configuration["ConnectionStrings:DefaultConnection"], 
+                      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                    )
+                  )
+                );
+                
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,7 +29,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+else 
+{
+  app.UseHttpsRedirection();
+}
 
 app.UseAuthorization();
 
